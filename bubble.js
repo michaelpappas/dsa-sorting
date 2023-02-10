cbdcd085f0e127b690f70eae3a522b40d82f7db1
michaelpappas <pappas.michael@gmail.com>
2023-02-10 11:59:26 -0800

    bubble sort passing

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+"use strict";
+/** takes in an input array and returns a sorted array */
+function bubbleSort(inputArray) {
+  for (let i = inputArray.length - 1; i > 0; i--) {
+    for (let j = 0; j <= i - 1; j++) {
+      if (inputArray[j] > inputArray[i]) {
+        let temp = inputArray[j];
+        inputArray[j] = inputArray[i];
+        inputArray[i] = temp;
+      }
+    }
+  }
+  return inputArray;
+}
+
+
 
 module.exports = bubbleSort;
